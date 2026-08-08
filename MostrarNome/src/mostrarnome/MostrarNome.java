5f1aa90c17f66cdfc1fabe487d8ebd01eaa8f0e5
Sofia Torres <Sofia.torres2004.st@gmail.com>
2026-08-08 18:10:30 -0400

    Add files via upload

---
+++ MostrarNome/src/mostrarnome/MostrarNome.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mostrarnome;
+
+/**
+ *
+ * @author sofia
+ */
+public class MostrarNome {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nome = "Sofia Torres de Souza Freitas"; 
+        System.out.println("Meu nome é: " + nome);
+    }
+    
+}
