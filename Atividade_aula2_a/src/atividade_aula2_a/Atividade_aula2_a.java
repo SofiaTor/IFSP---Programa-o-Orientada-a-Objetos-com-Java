cd0fe27d74dc5eef976d0382ce76ae8b8d1e8e7b
Sofia Torres <Sofia.torres2004.st@gmail.com>
2026-08-08 18:13:07 -0400

    Add files via upload

---
+++ Atividade_aula2_a/src/atividade_aula2_a/Atividade_aula2_a.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade_aula2_a;
+
+/**
+ *
+ * @author sofia
+ */
+//Nome: Sofia Torres de Souza Freitas
+
+public class Atividade_aula2_a {
+    public static void main(String[] args) {
+       
+        // Declaração das variáveis
+        String nome = "Sofia Torres de Souza Freitas";   // variável do tipo texto
+        int idade = 21;             // variável inteira
+        double altura = 1.68;       // variável real (ponto flutuante)
+        
+        // Exibindo os valores
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+    }
+}
