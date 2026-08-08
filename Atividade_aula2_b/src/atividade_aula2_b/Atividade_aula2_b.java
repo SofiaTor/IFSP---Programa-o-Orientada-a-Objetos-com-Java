794b27d745b39c25aedad36a96ef0a4588f71b7d
Sofia Torres <Sofia.torres2004.st@gmail.com>
2026-08-08 18:14:39 -0400

    Add files via upload

---
+++ Atividade_aula2_b/src/atividade_aula2_b/Atividade_aula2_b.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade_aula2_b;
+
+/**
+ *
+ * @author sofia
+ */
+//Nome: Sofia Torres de Souza Freitas
+
+public class Atividade_aula2_b {
+    public static void main(String[] args) {
+        // Declaração das variáveis
+        String nome = "Seu Nome";
+        double nota1 = 7.5;
+        double nota2 = 8.0;
+        
+        // Cálculo da média
+        double media = (nota1 + nota2) / 2;
+        
+        // Exibindo os resultados
+        System.out.println("Nome: " + nome);
+        System.out.println("Média: " + media);
+    }
+    
+}
